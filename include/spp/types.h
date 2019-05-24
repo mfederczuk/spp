0f9ddce708c862d94d376183105bc2a1131cae86
Michael Federczuk <federczuk.michael@protonmail.com>
2019-05-24 21:15:29 +0200

    Added types header file

---
+++ include/spp/types.h
@@ -0,0 +1,34 @@
+/*
+ * Script Preprocessor.
+ * Copyright (C) 2019 Michael Federczuk
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+// include/spp/types.h
+/*
+ * Header file for typedefs and other stuff.
+ *
+ * Since: v0.1.0 2019-05-24
+ * LastEdit: 2019-05-24
+ */
+
+#ifndef H650786EB_6C4F_4EE3_9B80_0108CCF43F85
+#define H650786EB_6C4F_4EE3_9B80_0108CCF43F85
+
+#include <wchar.h>
+#include <stdbool.h>
+
+typedef wchar_t* wcstr;
+
+#endif
