466d7aabbf19d4d54cfdc7bb6ddc869059e64450
Michael Federczuk <federczuk.michael@protonmail.com>
2019-05-31 13:12:41 +0200

    Opening and closing stream

--- src/main.c
+++ src/main.c
@@ -25,7 +25,18 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <spp/utils.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
 #include <spp/spp.h>
+
+#define errprintf(msg, ...) fprintf(stderr, (msg), __VA_ARGS__)
+
+/* exit codes
+ * 48 - <path>: too many symbolic links encountered
+ * 49 - <path>: path name too long
+ */
 
 int main(int argc, char** argv) {
 	cstr file = NULL;
@@ -43,8 +54,79 @@ int main(int argc, char** argv) {
 	} else if(argc == 3 && strcmp(argv[1], "--") == 0) {
 		file = argv[2];
 	} else if(argc > 2) {
-		fprintf(stderr, "%s: too many arguments: %d\n", argv[0], argc - 2);
+		errprintf("%s: too many arguments: %d\n", argv[0], argc - 2);
 		return 4;
+	}
+
+	FILE* ins = NULL;
+
+	if(file != NULL) {
+		struct stat sb;
+		if(stat(file, &sb) != 0) {
+			switch(errno) {
+			case EACCES: {
+				errprintf("%s: permission denied\n", argv[0]);
+				return 77;
+			}
+			case EBADF:
+			case EFAULT:
+			case EOVERFLOW: {
+				errprintf("%s: input/output error\n", argv[0]);
+				return 74;
+			}
+			case ELOOP: {
+				errprintf("%s: %s: too many symbolic links encountered\n",
+				          argv[0], file);
+				return 48;
+			}
+			case ENAMETOOLONG: {
+				errprintf("%s: %s: path name too long\n", argv[0], file);
+				return 49;
+			}
+			case ENOENT:
+			case ENOTDIR: {
+				errprintf("%s: %s: no such file\n", argv[0], file);
+				return 24;
+			}
+			case ENOMEM: {
+				errprintf("%s: not enough memory\n", argv[0]);
+				return 100;
+			}
+
+			default: {
+				errprintf("%s: unknown error\n", argv[0]);
+				return 125;
+			}
+			}
+		}
+
+		if(!S_ISREG(sb.st_mode)) {
+			errprintf("%s: %s: not a file\n", argv[0], file);
+			return 25;
+		}
+
+		ins = fopen(file, "r");
+		if(ins == NULL) {
+			if(errno == ENOMEM) {
+				errprintf("%s: not enough memory\n", argv[0]);
+				return 100;
+			}
+			// I don't want to create a case for every possibility that errno could
+			// be, we're just gonna use the catchall exit code 1 and use perror to
+			// show a message (this is rarely gonna happen anyway)
+			perror(argv[0]);
+			return 1;
+		}
+	} else {
+		ins = stdin;
+	}
+
+	// TODO: work with stream
+
+	if(file != NULL && fclose(ins) == EOF) {
+		// same thing as with fopen(); too many errno possibilies
+		perror(argv[0]);
+		return 1;
 	}
 
 	return 0;
