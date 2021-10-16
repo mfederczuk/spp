497aaf1f322e130158885ab12b1f2965866500e4
Michael Federczuk <federczuk.michael@protonmail.com>
2021-10-16 16:35:29 +0200

    Update usage and version info texts
    Updated copyright years and links.

--- include/spp/usage.h
+++ include/spp/usage.h
@@ -1,6 +1,6 @@
 /*
  * Script Preprocessor.
- * Copyright (C) 2019 Michael Federczuk
+ * Copyright (C) 2019, 2021  Michael Federczuk
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
  * Macro definition for the --help usage summary.
  *
  * Since: v0.1.0 2019-06-07
- * LastEdit: 2019-06-07
+ * LastEdit: 2021-10-16
  */
 
 #ifndef _SPP_USAGE_H
@@ -32,16 +32,16 @@
 	"    If FILE is omitted, read input from stdin.\n" \
 	"\n" \
 	"    Options:\n" \
-	"      --help     show this summary and exit\n" \
-	"      --version  show version and legal information and exit\n" \
+	"      --help     display this summary and exit\n" \
+	"      --version  display version and legal information and exit\n" \
 	"\n" \
 	"    Exit Status:\n" \
-	"      (using CommonCodes v1.0.0\n" \
-	"          <https://speziil.github.io/commoncodes/v/1.0.0.html>)\n" \
+	"      (using CommonCodes v2 <https://mfederczuk.github.io/commoncodes/v2.html>)\n" \
 	"      48  too many symbolic links encountered\n" \
 	"      49  path name too long\n" \
 	"\n" \
-	"Report bugs to: <federczuk.michael@hotmail.com>\n" \
-	"GitHub repository: <https://github.com/SpEZiiL/spp>\n"
+	"Report bugs to: <federczuk.michael@protonmail.com>\n" \
+	"         or at: <https://github.com/mfederczuk/spp/issues>\n" \
+	"GitHub repository: <https://github.com/mfederczuk/spp>\n"
 
 #endif /* _SPP_USAGE_H */
